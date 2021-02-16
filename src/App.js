0fbbdb11e7b20c49150769fdabc38a778015a492
fabrogabi <fabrogabi@gmail.com>
2021-02-16 23:48:17 +0100

    a

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment, useState} from 'react';
+import Formulario from './components/Formulario';
+import Header from './components/Header';
 
 function App() {
+  const [busqueda,guardarBusqueda]=useState({pais:'',ciudad:''});
+  const {pais,ciudad}=busqueda;
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Fragment>
+      
+    <Header titulo={'Clima React'} />
+    <div className="contenedor-form">
+      <div className="container">
+        <div className="row">
+          <div className="col-m6-s12">
+            <Formulario 
+            busqueda={busqueda}
+            guardarBusqueda={guardarBusqueda}
+            />
+          </div>
+          <div className="col-m6-s12">
+            2
+          </div>
+        </div>
+      </div>
     </div>
+
+    </Fragment>
   );
 }
 
---
+++ src/components/Formulario.js
@@ -0,0 +1,83 @@
+import React,{useState} from 'react';
+
+const Formulario = ({busqueda,guardarBusqueda}) => {
+    
+  
+    const [error,guardarError]=useState(false);
+
+    //extraer ciudad y pais
+    const {ciudad , pais}=busqueda;
+
+    //funcion que coloca los elementos en el state
+    const handleChace= e => {
+        //actualizar state
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name]:e.target.value
+                       });}
+
+    //cuando el usuario da submit al formulario 
+    const handleSubmit= e =>{
+        e.preventDefault();
+        //validar 
+        if ( ciudad==='' || pais==='')
+        {
+            guardarError(true);  
+            return;
+        }
+        guardarError(false);
+        //pasarlo al componente principal
+
+    }
+
+
+
+    return (
+        <form
+        onSubmit={handleSubmit}
+        >
+            {error ? <p className="red darken-4 error">Todos los campos son obligatorios</p> : null }
+            <div className="input-field col s12">
+                <input 
+                type="text"
+                name="ciudad"
+                id="ciudad"
+                value={ciudad}
+                onChange={handleChace}
+                />
+                <label htmlFor="ciudad" >Ciudad: </label>
+            </div>
+            <div className="input-field col s12">
+                <select
+                name="pais"
+                id="pais"
+                value={pais}
+                onChange={handleChace}
+                >
+                    <option value="">--Seleccionae Pais--</option>
+                    <option value="US">--EEUU--</option>
+                    <option value="MX">--MEXICO--</option>
+                    <option value="AR">--ARGENTINA--</option>
+                    <option value="CO">--COLOMBIA--</option>
+                    <option value="CR">--COSTA RICA--</option>
+                    <option value="PE">--PERU--</option>
+
+                </select>
+                <label htmlFor="pais">: </label> 
+
+            </div>
+            <div className="input-fild col s12">
+                <input
+                    type="submit"
+                    value="buscar clima"
+                    className="waves-effect waves-light btn-large btn-block yellow accent-4"
+                />
+
+            </div>
+        </form>
+    
+    
+        );
+}
+ 
+export default Formulario;
